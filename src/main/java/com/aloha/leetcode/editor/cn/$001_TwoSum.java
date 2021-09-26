6f076fedf4e91e598e6b7f9eb4da81ccf0cbf936
aloooha <chulei@imac.local>
2021-09-27 00:53:46 +0800

    init

---
+++ src/main/java/com/aloha/leetcode/editor/cn/$001_TwoSum.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2021. All rights reserved.
+ * @LeetCode Question: 两数之和
+ * @Author: chulei
+ * @Date: 2021/9/27 上午12:12
+ */
+
+
+package com.aloha.leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class $001_TwoSum{
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+
+
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+// Related Topics 数组 哈希表 👍 12210 👎 0
